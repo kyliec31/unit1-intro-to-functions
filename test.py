5cc1689fae13f95231050e507b4a3e48e2516d92
kyliec31 <kyliec31@nycstudents.net>
2026-01-28 09:24:03 -0500

    1/28

---
+++ test.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+turtle.done()

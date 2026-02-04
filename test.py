6a477d21253d642a99fd04ff072ce1e653653f87
kyliec31 <kyliec31@nycstudents.net>
2026-02-04 09:22:32 -0500

    2/4

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import turtle
+""" import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
+t.speed(100) """
 
 """ def square(t):
     for i in range(4):
@@ -50,20 +51,94 @@ def addSquares(iRange):
         length += 25
 addSquares(5) """
 
+""" rotate = 90
 def square(x,y):
     for i in range(4):
         t.forward(x)
         t.left(y)
 
-square(t)
-
-for i in range(60):
-    def addSquares(iRange):
+def addSquares(iRange):
+    length = 5
+    for i in range(iRange):
         square(length, 90)
-        length+=5
-    t.left(5)
+        length += 5
+        t.right(5)
+addSquares(60)
+ """
+""" rotate = 108
+def triangle(x,y):
+    for i in range(3):
+        t.forward(x)
+        t.left(y)
 
+def addTriangles(iRange):
+    length = 100
+    for i in range(iRange):
+        triangle(length,144)
+        length += 5
+addTriangles(2)
+        """
+""" x = 3
+y = float(3)
+print(x,y) """
 
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
 
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(values[0])
+    print(values[6]) """
 
-turtle.done
+""" values = [1,2,5,7]
+
+print(values)
+for i in values:
+    print(values[0])
+ """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+""" 
+sentence = input("Input a sentence") """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+ """
+""" def discount(age, isMember, isResident):
+    if age <= 12 or age >=65:
+        print("discount")
+
+    elif isMember == True:
+        print("discount")
+    elif isResident == True:
+        print("discount")
+    else:
+        print("no discount")
+discount(30, False, False) """
+
+""" x = "test"
+print(f"hello {x}") """
+""" 
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+x = 11
+value = [1,2,3,4,5,6,7,8,9,10]
+if value is [1,3,5,7,9]:
+    print("odd")
+if value is 
+
